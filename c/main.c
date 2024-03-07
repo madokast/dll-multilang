5a73a88b51d41e10868ab9fe18bce352fe9ff3ec
runxiao zhao <578562554@qq.com>
2024-03-07 14:41:46 +0800

    c

--- c/main.c
+++ c/main.c
@@ -3,27 +3,30 @@
 
 typedef int (*c_add)(int, int); // Example signature, adjust as needed
 
-// gcc -o3 -o .\c\main.exe .\c\main.c
-int main() {
-    printf("LoadLibrary ./c_add.dll\n");
-    HINSTANCE hDll = LoadLibrary("./c_add.dll");
+void run(char* dll_path) {
+    printf("LoadLibrary %s\n", dll_path);
+    HINSTANCE hDll = LoadLibrary(dll_path);
     if (hDll == NULL) {
         printf("Failed to load DLL\n");
-        return 1;
+        return;
     }
-    printf("GetProcAddress Add in ./c_add.dll\n");
+    printf("GetProcAddress Add in %s\n", dll_path);
     c_add myFunction = (c_add) GetProcAddress(hDll, "Add");
     if (myFunction == NULL) {
         printf("Failed to get function address\n");
         FreeLibrary(hDll);
-        return 1;
+        return;
     }
-    printf("Call Add in ./c_add.dll\n");
+    printf("Call Add in %s\n", dll_path);
     int result = myFunction(42, 33); // Example argument
     printf("Result: %d\n", result);
 
     FreeLibrary(hDll);
-    printf("FreeLibrary ./c_add.dll\n");
-    return 0;
+    printf("FreeLibrary %s\n", dll_path);
+}
+ 
+// gcc -o3 -o .\c\main.exe .\c\main.c
+int main() {
+    run("./c_add.dll");
 }
 
