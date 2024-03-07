a7816fba59b59aec6a4e83228900adb887656678
runxiao zhao <578562554@qq.com>
2024-03-07 14:49:53 +0800

    py

---
+++ py/main.py
@@ -0,0 +1,14 @@
+from ctypes import windll, c_int
+
+def run(dll_path):
+    print(f"windll.LoadLibrary {dll_path}")
+    dll = windll.LoadLibrary(dll_path)
+    print(f"Call dll.add {dll_path}")
+    s = dll.Add(c_int(20), c_int(30))
+    print(f"result {s}")
+
+
+if __name__ == '__main__':
+    import os
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    run(os.path.join(base_dir, 'c/c_add.dll'))
